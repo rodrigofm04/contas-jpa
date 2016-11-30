278c57597045bc52147df53cab4fd12b5210242d
Rodrigo F. Melo <rodrigofm@tst.jus.br>
2016-11-30 17:56:35 -0200

    Criando ContaTeste

---
+++ src/test/java/br/com/caelum/teste/ContaTest.java
@@ -0,0 +1,39 @@
+package br.com.caelum.teste;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class ContaTest {
+
+	@Test
+	public void insereContaNoBanco() {
+		Conta conta = new Conta();
+		conta.setTitular("Rodrigo Melo");
+		conta.setBanco("Itau");
+		conta.setAgencia("013");
+		conta.setNumero("5561");
+
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+
+		em.persist(conta);
+
+		em.getTransaction().commit();
+		em.close();
+
+	}
+
+	@Test
+	public void buscaContaNoBanco() {
+		EntityManager em = new JPAUtil().getEntityManager();
+		Conta conta = em.find(Conta.class, 169);
+		assertEquals("Jose dos Santos", conta.getTitular());
+	}
+
+}
